878454554586d38369817b6de6b62fa472fee674
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2022-11-26 15:03:55 +0100

    fix animation

--- src/components/ColorBubble.jsx
+++ src/components/ColorBubble.jsx
@@ -11,7 +11,8 @@ export const ColorBubble = ({ size, top, left, right, backgroundColor }) => {
         borderRadius: "50%",
         position: "absolute",
         zIndex: "-1",
-        transition: "all .5s"
+        transition: "all .5s",
+        animation: "fadeIn 1s ease-in"
       }}
     />
   );
