b8e9baf7157b8aeb0b1c398f641adc03a07c703f
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2022-12-04 15:33:08 +0100

    add colorPicker transition

--- src/components/ColorButton.jsx
+++ src/components/ColorButton.jsx
@@ -2,16 +2,17 @@ export const ColorButton = ({ color, setColor, backgroundColor, colorName }) => 
   const isSelected = color === backgroundColor
   return (
     <div onClick={() => setColor(backgroundColor)} className="colorButton">
-      <div
-        className="color"
-        style={{
-          backgroundColor,
-        }}
-        />
       <p
       style={{
         fontWeight: isSelected && "600"
       }}>{colorName}</p>
+      <div
+        className="color"
+        style={{
+          backgroundColor,
+          width: isSelected ? "100%" : ".8rem",
+        }}
+        />
     </div>
   );
 };
